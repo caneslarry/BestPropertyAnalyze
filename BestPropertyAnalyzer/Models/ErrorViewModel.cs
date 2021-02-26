083300ad482bc576d256891bc74610fed8c6f2f6
larry <caneslarry@gmail.com>
2021-02-26 12:03:42 -0500

    init

---
+++ BestPropertyAnalyzer/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace BestPropertyAnalyzer.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
