ea5e8aa7429c079dbcfa499b2d9b8ff962192e2d
larry <caneslarry@gmail.com>
2021-03-06 20:14:56 -0500

    - Added Google - Added chart

--- BestPropertyAnalyzer/Controllers/HomeController.cs
+++ BestPropertyAnalyzer/Controllers/HomeController.cs
@@ -6,16 +6,43 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using BestPropertyAnalyzer.Models;
+using System.Collections.Specialized;
+using System.Web;
+using System.Net.Http;
+using System.Net;
+using System.Xml;
+using Zillow.Services;
+using Zillow.Services.Schema;
+using System.Text.Json;
+using System.Collections;
+using System.Net.Http.Json;
+using System.Text.Json.Serialization;
+using System.Net;
+using Newtonsoft.Json;
+using System.Xml.Linq;
+using System.Xml.Serialization;
 
 namespace BestPropertyAnalyzer.Controllers
 {
     public class HomeController : Controller
     {
+        public const double RestEstimate = 1500.00;
+        public const double Mortgage = 1100.00;
+        public const double Tax = 00.00;
+        public const double Insurance = 100.00;
+
+        private string[] ignoreList = { "PropertyRecords", "details", "Building_Area" };
+
+        private const string ZWSID = "X1-ZWz16rr2tjewwb_9hc6e";
+
+        public const string ZillowRoot = "http://www.zillow.com/webservice/GetSearchResults.htm";
+
         private readonly ILogger<HomeController> _logger;
 
         public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
+
         }
 
         public IActionResult Index()
@@ -33,5 +60,55 @@ namespace BestPropertyAnalyzer.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        [HttpPost]
+        public ListingData GetPropertyData(string address, string citystatezip)
+        {
+            var jsonData = string.Empty;
+
+            var url = "https://qvmservices-test.quantarium.com/QDataService/QueryPropertiesByAddress?u=KnoxHoldings-Test&k=fP0uwn;K73Jgs=dg&address=" + address + "&citystate_zip=" + citystatezip;
+
+            ListingData[] data = null;
+            ListingData house = null;
+
+
+            using (var webClient = new WebClient())
+            {
+                try
+                {
+                    jsonData = webClient.DownloadString(url);
+
+                    //TODO: jsonData can be null
+
+                    data = System.Text.Json.JsonSerializer.Deserialize<ListingData[]>(jsonData);
+
+                    house = data[0];
+
+
+                }
+                catch (Exception e)
+                {
+                    ;
+                }
+
+
+            }
+
+            url = "https://qvmservices-test.quantarium.com/QDataService/GetPropertyRecordsXml?u=KnoxHoldings-Test&k=fP0uwn;K73Jgs=dg&id=" + house.id;
+
+            XDocument xmlDoc = XDocument.Load(url);
+
+            foreach (var node in xmlDoc.Descendants())
+            {
+                if (!ignoreList.Contains<string>(node.Name.LocalName))
+                {
+                    house.Details += node.Name.LocalName + ": " + node.Value + "<br/>";
+                }
+            }
+
+            return house;
+
+        }
+
     }
 }
---
+++ BestPropertyAnalyzer/Models/ListingData.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Xml.Serialization;
+
+namespace BestPropertyAnalyzer.Models
+{
+    [XmlRoot("parcel_snapshot")]
+    public class ListingData
+    {
+        public string id { get; set; }
+
+        public string address { get; set; }
+
+        public string apn { get; set; }
+
+        public string county { get; set; }
+
+        public float latitude { get; set; }
+
+        public float longitude { get; set; }
+
+        public string city { get; set; }
+
+        public string owner { get; set; }
+
+        public string Details { get; set; }
+
+
+        //jsonData = "{\"address\":\"863 MASSACHUSETTS AVE APT 15, CAMBRIDGE, MA 02139\",\"apn\":\"CAMB M:00117 L: 0005700015\",\"city\":\"CAMBRIDGE\",\"county\":\"MIDDLESEX\",\"id\":\"65891272\",\"latitude\":42.367708,\"longitude\":-71.107172,\"matching_reason\":0}";
+
+        public ListingData()
+        {
+        }
+    }
+}
